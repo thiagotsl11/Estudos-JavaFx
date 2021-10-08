7c4d11de77a87cf0812bc7336f23eefb460818de
Thiago <thiagotsl11@outlook.com>
2021-10-08 13:45:13 -0300

    Estudos JavaFX

---
+++ src/basico/primeiroFx.java
@@ -0,0 +1,16 @@
+package basico;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class primeiroFx extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module exerciciosfx {
+	
+	// Para poder acessar os comandos de controle
+	requires javafx.controls;
+	// Para poder abrir tudo que fiz no console
+	opens basico;
+}
